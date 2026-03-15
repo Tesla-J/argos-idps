36e40b76f0f339fe8d91e54749f15d7ca570a9b9
Rafael Marcos <rafix995@gmail.com>
2026-03-15 04:25:30 +0100

    ANSI color constants

---
+++ Argos_IDPS/src/main/kotlin/colors/PrintColors.kt
@@ -0,0 +1,7 @@
+package ao.argosidps.colors
+
+const val RED = "\u001b[91m"
+const val GREEN = "\u001b[92m"
+const val YELLOW = "\u001b[93m"
+const val BLUE = "\u001b[94m"
+const val RESET = "\u001b[0m"
